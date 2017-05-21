c51109fb3e3a82004fb05ee45e932afe998d7ae5
Kristel Tjintjelaar <kristel@interactivexperience.nl>
2017-05-21 21:55:59 +0200

    Added feedback fixes'

--- web/themes/custom/xxs/js/xxs.js
+++ web/themes/custom/xxs/js/xxs.js
@@ -9,8 +9,110 @@
 
   $(window).on('load', function () {
     $('.paragraph--type--text .field--name-field-text, .paragraph--type--two-column-text, .paragraph--type-project-results').addClass('container');
+    $('.path-frontpage .section:nth-child(6)').removeClass('fp-noscroll');
     $('.field--item:nth-child(2) .paragraph').attr('id', 'scroll-to-paragraph');
     $('.page-node-type-like .paragraph--type--header .header-content').prepend($('.page-node-type-like .field--name-node-post-date'));  
+    $('.field--name-field-paragraphs').append($('#block-views-block-likes-block-1'));  
+    	// Fullpage
+	
+	$(document).ready(function() {
+  	$('#block-views-block-likes-block-1').addClass('section');
+    $('#fullpage').fullpage({
+		//Navigation
+		menu: '#menu',
+		lockAnchors: false,
+		anchors:['firstPage', 'secondPage'],
+		navigation: false,
+		navigationPosition: 'right',
+		navigationTooltips: ['firstSlide', 'secondSlide'],
+		showActiveTooltip: false,
+		slidesNavigation: false,
+		slidesNavPosition: 'bottom',
+
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 700,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 1000,
+		scrollBar: true,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: true,
+		continuousVertical: false,
+		continuousHorizontal: false,
+		scrollHorizontally: false,
+		interlockedSlides: false,
+		dragAndMove: false,
+		offsetSections: false,
+		resetSliders: false,
+		fadingEffect: false,
+		normalScrollElements: '#element1, .element2',
+		scrollOverflow: true,
+		scrollOverflowReset: false,
+		scrollOverflowOptions: null,
+		touchSensitivity: 15,
+		normalScrollElementTouchThreshold: 5,
+		bigSectionsDestination: null,
+
+		//Accessibility
+		keyboardScrolling: true,
+		animateAnchor: true,
+		recordHistory: true,
+
+		//Design
+		controlArrows: true,
+		verticalCentered: true,
+		sectionsColor : ['#ccc', '#fff'],
+		paddingTop: '0',
+		paddingBottom: '0',
+		fixedElements: '#header, .footer',
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		responsiveSlides: false,
+		parallax: false,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+		//Custom selectors
+		sectionSelector: '.section',
+		slideSelector: '.slide',
+
+		lazyLoading: true,
+
+		//events
+		onLeave: function(index, nextIndex, direction){},
+		afterLoad: function(anchorLink, index){},
+		afterRender: function(){},
+		afterResize: function(){},
+		afterResponsive: function(isResponsive){},
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+	});
+});
+      // Scroll to top
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+}
     
     // Scroll to anchor
     $('a[href^="."]').on('click',function (e) {
@@ -25,32 +127,12 @@
 	        window.location.hash = target;
 	    });
 	});
-    
-    // Change value of select box
-    document.getElementById('edit-field-category-target-id').options[0].innerHTML = 'Filter work';
-    
-    // Scroll to top
-  if ($('#back-to-top').length) {
-      var scrollTrigger = 100, // px
-          backToTop = function () {
-              var scrollTop = $(window).scrollTop();
-              if (scrollTop > scrollTrigger) {
-                  $('#back-to-top').addClass('show');
-              } else {
-                  $('#back-to-top').removeClass('show');
-              }
-          };
-      backToTop();
-      $(window).on('scroll', function () {
-          backToTop();
-      });
-      $('#back-to-top').on('click', function (e) {
-          e.preventDefault();
-          $('html,body').animate({
-              scrollTop: 0
-          }, 700);
-      });
-  }
+	
+	    
+  // Change value of select box
+  //document.getElementById('edit-field-category-target-id').options[0].innerHTML = 'Filter work';
+	
+
   
   
   })
